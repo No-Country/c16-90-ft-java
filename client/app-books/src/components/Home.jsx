4da830a34ea9cdab3b8df2ec067218a4ba15ee83
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-02-14 20:47:53 -0300

    cambios

--- client/app-books/src/components/Home.jsx
+++ client/app-books/src/components/Home.jsx
@@ -1,35 +1,121 @@
-import React from "react";
-import Cards from "./Cards";
+import React, { useState, useEffect } from "react";
 import SectionBooks from "./SectionBooks";
 
 const category = [
-  { name: "Aventura" },
-  { name: "Ciencia Ficción" },
-  { name: "Policiaca" },
-  { name: "Terror y suspenso" },
-  { name: "Romantica" },
-  { name: "Poesá" },
+  { name: "Young Adult" },
+  { name: "Fiction" },
+  { name: "Science Fiction" },
+  { name: "Dystopia" },
+  { name: "Fantasy" },
+  { name: "Historical" },
+  { name: "Historical Fiction" },
+  { name: "Academic" },
+  { name: "School" },
+  { name: "Romance" },
+  { name: "Paranormal" },
+  { name: "Vampires" },
+  { name: "Literature" },
+  { name: "Politics" },
+  { name: "Novels" },
+  { name: "Read For School" },
+  { name: "Classics" },
 ];
 
 const Home = () => {
+  const [currentValue, setCurrentValue] = useState("");
+  const [categorySelect, setCategorySelect] = useState("");
+
+  const [books, setBooks] = useState([]);
+
+  const [filterValue, setFilterValue] = useState([]);
+  const [message, setMessage] = useState("");
+
+  //pedir datos a la api
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch(
+        "https://example-data.draftbit.com/books?_limit=50"
+      );
+
+      const data = await response.json();
+
+      if (data) {
+        setBooks(data);
+      }
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Lógica para realizar la búsqueda
+    const lowerCaseValue = currentValue.toLowerCase();
+
+    const filteredBooks = books.filter((elem) => {
+      if (lowerCaseValue) {
+        return (
+          elem.title.toLowerCase().includes(lowerCaseValue) ||
+          elem.authors.toLowerCase().includes(lowerCaseValue)
+        );
+      } else {
+        return [...books]; // Si ninguna condición es verdadera, incluir todos los libros
+      }
+    });
+
+    // Actualizar el estado con los libros filtrados
+    setFilterValue(filteredBooks);
+
+    // Verificar si se encontraron libros
+    if (filteredBooks.length === 0) {
+      setMessage("No se encontraron libros");
+    } else {
+      setMessage(""); // Reiniciar el mensaje si se encontraron libros
+    }
+  }
+
+  useEffect(() => {
+    const lowerCaseCategorySelect = categorySelect.toLowerCase();
+
+    const filteredBooks = books.filter((elem) => {
+      if (lowerCaseCategorySelect) {
+        return elem.genres.toLowerCase().includes(lowerCaseCategorySelect);
+      }
+      return true;
+    });
+
+    // Limpiar el mensaje cuando cambie la categoría
+    setMessage("");
+
+    setFilterValue(filteredBooks);
+  }, [categorySelect, books]);
+
+  //llamada de api
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <main>
-      <nav className="w-full flex gap-2">
-        <ul className="flex gap-4">
+      <nav className="w-full flex flex-col gap-2">
+        <ul className="flex gap-4 flex-wrap">
           {category.map((items, index) => (
             <li
               key={index}
               className="py-2 px-3 text-sm bg-muted rounded-md flex items-center gap-x-1 bg-slate-200 transition cursor-pointer hover:bg-slate-400"
+              onClick={(e) => setCategorySelect(e.target.innerText)}
             >
               {items.name}
             </li>
           ))}
         </ul>
-        <form className="flex items-center  flex-1">
+        <form className="flex max-w-xl" onSubmit={handleSubmit}>
           <input
             className="flex h-10 border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full  rounded-lg rounded-r-none focus-visible:ring-transparent pr-8"
-            placeholder="Search book"
-            value=""
+            placeholder="Search book - authors"
+            value={currentValue}
+            onChange={(e) => setCurrentValue(e.target.value)}
           />
           <button
             className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-sky-700 text-white hover:bg-sky-700/80 h-10 px-4 py-2 rounded-l-none"
@@ -53,8 +139,7 @@ const Home = () => {
           </button>
         </form>
       </nav>
-      {/* <Cards /> */}
-      <SectionBooks />
+      <SectionBooks books={filterValue} message={message} />
     </main>
   );
 };
--- client/app-books/src/components/SectionBooks.jsx
+++ client/app-books/src/components/SectionBooks.jsx
@@ -1,39 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Cards from "./Cards";
 
-const SectionBooks = () => {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    try {
-      const response = await fetch(
-        "https://example-data.draftbit.com/books?_limit=50"
-      );
-
-      const data = await response.json();
-
-      if (data) {
-        setBooks(data);
-        console.log(books);
-      }
-    } catch (error) {}
-  };
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
+const SectionBooks = ({ books, message }) => {
   return (
-    <div className="grid grid-cols-3 grid-rows-1 gap-4 mt-10">
-      {books.map((book, index) => (
-        <Cards
-          title={book.title}
-          authors={book.authors}
-          genres={book.genres}
-          description={book.description}
-          image={book.image_url}
-        />
-      ))}
+    <div className="mt-10">
+      <div className="grid grid-cols-3 grid-rows-1 gap-4 mt-10">
+        {message && <p>{message}</p>}
+        {books.map((book, index) => (
+          <Cards
+            key={index}
+            title={book.title}
+            authors={book.authors}
+            genres={book.genres}
+            description={book.description}
+            image={book.image_url}
+          />
+        ))}
+      </div>
     </div>
   );
 };
