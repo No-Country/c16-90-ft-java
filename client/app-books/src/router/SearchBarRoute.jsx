d93f003931db8b08792f2fec95c58d0cbdc738c0
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-02-15 23:44:10 -0300

    retoques

--- client/app-books/src/router/SearchBarRoute.jsx
+++ client/app-books/src/router/SearchBarRoute.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SearchBar from '../Components/SearchBar'
-import HomePage from '../Components/HomePage'
-import LoginPage from '../pages/LoginPage'
-import SignupPage from '../pages/SignupPage'
-import Dashboard from '../Components/Dashboard'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+import HomePage from "../components/HomePage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import Dashboard from "../components/Dashboard";
 
 const SearchBarRoute = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<SearchBar/>}>
-          <Route index element={<HomePage/>}/>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/signup' element={<SignupPage/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path="/" element={<SearchBar />}>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default SearchBarRoute
+export default SearchBarRoute;
