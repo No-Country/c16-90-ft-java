e059f3db2d5b97103f025a4ba392f852283bf458
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-02-19 20:43:18 -0300

    cambios

--- client/app-books/src/router/SearchBarRoute.jsx
+++ client/app-books/src/router/SearchBarRoute.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
 import LoginPage from "../pages/LoginPage";
-import Dashboard from "../components/Dashboard";
 import Home from "../components/Home/Home";
 import SignupPage from "../pages/SignupPage";
-import Tables from "../pages/Tables";
-import Settings from "../pages/Settings";
-import PageTitle from "../components/Admin/PageTitle";
 import DashboardAdminRoute from "./DashboardAdminRoute";
 
 const SearchBarRoute = () => {
