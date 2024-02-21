7d0c7973bc572c09c19623134903ea1f98d14296
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-02-21 20:29:21 -0300

    cambios

--- client/app-books/src/App.jsx
+++ client/app-books/src/App.jsx
@@ -1,13 +1,4 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Adventure from "./components/Adventure";
-import SciencieFiction from "./components/SciencieFiction";
-import Detective from "./components/Detective";
-import HorrorSuspense from "./components/HorrorSuspense";
-import Romantic from "./components/Romantic";
-import Poem from "./components/Poem";
-
 import SearchBarRoute from "./router/SearchBarRoute";
 import DashboardAdminRoute from "./router/DashboardAdminRoute";
 
@@ -15,7 +6,6 @@ function App() {
   return (
     <main>
       <SearchBarRoute />
-
       <DashboardAdminRoute />
     </main>
   );
